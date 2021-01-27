b8556a3c360f6f3d81ee94248c29b7d8c28ae343
Dainius Remeikis <dainius.remeikis@gmail.com>
2021-01-27 19:15:29 +0200

    Add project files.

---
+++ Database/Products/Batai.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210127_ShoeStore.Database.Products
+{
+    public class Batai
+    {
+        public int BataiId { get; set; }
+        public string BatuPavadinimas { get; set; }
+        public string BatųTipas { get; set; }
+        public double BatuKaina { get; set; }
+    }
+}
---
+++ Database/Products/BatuDydziai.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210127_ShoeStore.Database.Products
+{
+    class BatuDydziai
+    {
+
+        public int BataiId { get; set; }
+        public int BatuDydis { get; set; }
+        public int BatuKiekis { get; set; }
+    }
+}
